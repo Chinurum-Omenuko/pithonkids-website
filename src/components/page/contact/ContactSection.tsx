f1b99b8199c442dc541d8ca7a86d9509f58b39dd
Chinurum-Omenuko <omen0003@algonquinlive.com>
2025-06-23 23:16:20 -0400

    adds phone number href and style

--- src/components/page/contact/ContactSection.tsx
+++ src/components/page/contact/ContactSection.tsx
@@ -81,10 +81,11 @@ export default function ContactForm() {
           <div className="space-y-6 text-gray-700 text-sm">
             <div className="flex items-center gap-4">
               <Phone className="w-5 h-5 text-gray-500" />
-              <div className="phone flex flex-col">
-                <p> <a href="tel:6474099936">+1 (647) 409-9936</a></p>
-                <p> <a href="tel:6132914770">+1 (613) 291-4770</a></p>
+              <div className="phone flex flex-col gap-4">
+                <p className="underline"> <a href="tel:6474099936">+1 (647) 409-9936</a></p>
+                <p className="underline"> <a href="tel:6132914770">+1 (613) 291-4770</a></p>
               </div>
+              
             </div>
             <div className="flex items-center gap-4">
               <Mail className="w-5 h-5 text-gray-500" />
