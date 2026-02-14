42f6944273c2769fda659903b05c9fcb6d70f289
Chinurum-Omenuko <omen0003@algonquinlive.com>
2026-02-13 22:02:41 -0500

    reconfig

--- .history/next.config_20250623143135.ts
+++ .history/next.config_20250623143135.ts
@@ -1,18 +1,19 @@
 import type { NextConfig } from "next";
+import { i18n } from "./next-i18next.config";
 
 const nextConfig: NextConfig = {
+  i18n,
   reactStrictMode: true,
   swcMinify: true,
   images: {
     formats: ['image/webp'],
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true,  // ⬅ disables type checking during build
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // ⬅ disables ESLint errors/warnings from blocking builds
   },
 };
-
 
 export default nextConfig;
--- .history/next.config_20260213220109.ts
+++ .history/next.config_20260213220109.ts
@@ -1,18 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  i18n,
   reactStrictMode: true,
   swcMinify: true,
   images: {
     formats: ['image/webp'],
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true,  // ⬅ disables type checking during build
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // ⬅ disables ESLint errors/warnings from blocking builds
   },
 };
-
 
 export default nextConfig;
